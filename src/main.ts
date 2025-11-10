12061739ba6ebce9e669380c4a82155b9821bdd2
Joel <joestrell@alumnos.unex.es>
2025-11-10 16:05:32 +0100

    GG-94 - CA01: Notificacion artista-seguidor

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
+// src/main.ts
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NotificationsModule } from './notifications/notifications.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+    const app = await NestFactory.create(NotificationsModule);
+
+    // Activamos validación automática para DTOs
+    app.useGlobalPipes(new ValidationPipe({
+        whitelist: true, // elimina propiedades no declaradas en DTO
+        forbidNonWhitelisted: true, // lanza error si vienen propiedades extra
+        transform: true, // transforma tipos automáticamente según DTO
+    }));
+
+    const port = 3000;
+    await app.listen(port);
+    console.log(`🚀 Notifications service is running on http://localhost:${port}`);
 }
 bootstrap();
